50a0334a6937e505609bf078b9f7d4e620edfdd0
migueszitle111 <124220733+migueszitle111@users.noreply.github.com>
2023-05-23 16:07:26 -0700

    Add files via upload

---
+++ SENSOR 016 (TEMP AND HUMIDITY)/dhtcodigo.py
@@ -0,0 +1,16 @@
+from machine import Pin
+import utime 
+from dht import DHT11
+
+dhtPin = Pin(20, Pin.IN , Pin.PULL_DOWN)
+
+while True:
+    utime.sleep(2)
+    dhtSensor = DHT11(dhtPin)
+    
+    temp_value = dhtSensor.temperature
+    
+    hum_value = dhtSensor.humidity
+    
+    print("Temperature : ", temp_value, "Degree Celcius")
+    print("Relative Humidity : ", hum_value, " %", "\n")
