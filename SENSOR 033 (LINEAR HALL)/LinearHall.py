3a43e5659ad2be5340d236968227edf83017c85d
migueszitle111 <124220733+migueszitle111@users.noreply.github.com>
2023-05-23 16:38:03 -0700

    Add files via upload

---
+++ SENSOR 033 (LINEAR HALL)/LinearHall.py
@@ -0,0 +1,9 @@
+from machine import Pin, ADC
+import time
+
+digital = Pin(18, Pin.IN)
+analog = ADC(26)
+
+while True:
+    print(str(digital.value()) + ' - ' + str(analog.read_u16()))
+    time.sleep(1)s
