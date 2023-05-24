bd8716a6e1b46924a159ad03b730088e9c2874bb
migueszitle111 <124220733+migueszitle111@users.noreply.github.com>
2023-05-23 17:54:41 -0700

    Add files via upload

---
+++ SENSOR 001 (HEARTBEAT)/heartbeat.py
@@ -0,0 +1,11 @@
+from machine import ADC
+from utime import sleep
+
+sensor = ADC(26)
+
+conversion_factor = 3.3 / (65535)
+
+while True:
+        lectura = sensor.read_u16() * conversion_factor
+	print(lectura)
+        sleep(2)
